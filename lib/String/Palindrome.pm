a7fa891530d4019b13916464ac516e9a7d9e649d
Adam Lesperance <lespea@gmail.com>
2010-08-12 03:03:15 -0500

    Switched over to Dist::Zilla

--- lib/String/Palindrome.pm
+++ lib/String/Palindrome.pm
@@ -10,35 +10,40 @@ use vars qw(@ISA @EXPORT_OK);
 @EXPORT_OK  = qw(is_palindrome);  # symbols to export on request
 
 
-=head1 NAME
-
-String::Palindrome - Determine if a string is a palindrome
-
-=head1 VERSION
-
-Version 0.01
-
-=cut
-
-use vars qw($VERSION);
-$VERSION = '0.01';
+#ABSTRACT: Determine if a string is a palindrome
 
 
 =head1 SYNOPSIS
 
     use String::Palindrome qw/ is_palindrome /;
 
+    #  Simple strings
     my $is     = 'abccba';
     my $is_not = 'abcdef';
 
-    is_palindrome( $is     )  = 1;
-    is_palindrome( $is_not )  = 0;
+    is_palindrome( $is      );  # 1
+    is_palindrome( $is_not  );  # 0
+    is_palindrome( {a => 1} );  # undef
 
-=head1 EXPORTABLE
+
+    #  Arrays
+    my @is     = qw/ a ab cc ab a /;
+    my @is_not = qw/ a ab cc a ba /;
+
+    is_palindrome(  @is     );  # 1
+    is_palindrome( \@is     );  # 1
+    is_palindrome(  @is_not );  # 0
+    is_palindrome( \@is_not );  # 0
+    is_palindrome( []       );  # undef
+
+=head1 EXPORTS
+
+=for :list
+* is_palindrome
 
 =cut
 
-=head2 is_palindrome
+=func is_palindrome
 
 Returns true if the string is a palindrome.  Currently, passing a ref will also
 cause an undef to return and a blank string is reported as not a palindrome.
@@ -54,60 +59,5 @@ sub is_palindrome {
     return  ($str ne q{}  and  $str eq scalar reverse $str)  ?  1  :  0;
 }
 
-=head1 AUTHOR
-
-Adam Lesperance, C<< <lespea at cpan.org> >>
-
-=head1 BUGS
-
-Please report any bugs or feature requests to C<bug-string-palindrome at rt.cpan.org>, or through
-the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=String-Palindrome>.  I will be notified, and then you'll
-automatically be notified of progress on your bug as I make changes.
-
-
-
-
-=head1 SUPPORT
-
-You can find documentation for this module with the perldoc command.
-
-    perldoc String::Palindrome
-
-
-You can also look for information at:
-
-=over 4
-
-=item * RT: CPAN's request tracker
-
-L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=String-Palindrome>
-
-=item * AnnoCPAN: Annotated CPAN documentation
-
-L<http://annocpan.org/dist/String-Palindrome>
-
-=item * CPAN Ratings
-
-L<http://cpanratings.perl.org/d/String-Palindrome>
-
-=item * Search CPAN
-
-L<http://search.cpan.org/dist/String-Palindrome/>
-
-=back
-
-
-=head1 LICENSE AND COPYRIGHT
-
-Copyright 2009 Adam Lesperance.
-
-This program is free software; you can redistribute it and/or modify it
-under the terms of either: the GNU General Public License as published
-by the Free Software Foundation; or the Artistic License.
-
-See http://dev.perl.org/licenses/ for more information.
-
-
-=cut
 
 1; # End of String::Palindrome
--- t/01-func_test.t
+++ t/01-func_test.t
@@ -3,25 +3,62 @@
 use Test::More;
 use String::Palindrome qw/ is_palindrome /;
 
-my @ok    = qw/ qwerrewq 12344321 axc3223cxa a aa aaaaaaa aaabaaa /;
-my @nok   = qw/ asdf fejal aabab asdf 1343 /; push @nok, q{};
+use Data::Dumper;
+
+
+#  Simple checks
+my @ok    = qw/ qwerrewq 123454321 12344321 axc3223cxa a aa aaaaaaa aaabaaa /;
+my @nok   = qw/ asdf fejal aabab asdf 1343 /;
 my @undef = ([], {a => 1}, undef);
 
-# POD examples
+
+#  Array checks
+my @ok_ref = (@ok,   reverse @ok);
+my @nok_ref = (@nok, reverse @nok);
+
+push @ok_ref,  [split //, $_]  for  @ok;
+push @nok_ref, [split //, $_]  for  @nok;
+
+push @ok_ref,  [qw/ hi  bye  ted  bye hi]/, qw/ hello  world  world  hello /];
+push @nok_ref, [qw/ hi  bye  ted  bye ih]/, qw/ hello  world  hello /];
+
+
+#  POD examples
 push @ok,  'abccba';
 push @nok, 'abcdef';
 
-plan tests => scalar @ok + scalar @nok + scalar @undef;
+
+#  Seperate "blank" check
+push @nok, '';
 
 
+#  Plan our tests
+plan tests => @ok + (@ok_ref * 2) + @nok + (@nok_ref * 2) + @undef;
+
+
+#  Test OKs
 for  my $ok  (@ok) {
     is(is_palindrome($ok), 1, "($ok) not reported as a palindrome");
 }
 
+for  my $ok  ( @ok_ref ) {
+    is(is_palindrome($ok),  1, sprintf('(%s) [array_ref] not reported as a palindrome', Dumper $ok));
+    is(is_palindrome(@$ok), 1, sprintf('(%s) [array] not reported as a palindrome',     Dumper $ok));
+}
+
+
+#  Test Not OKs
 for  my $nok  (@nok) {
     is(is_palindrome($nok), 0, "($nok) reported as a palindrome");
 }
 
+for  my $nok  ( @nok_ref ) {
+    is(is_palindrome($nok),  0, sprintf('(%s) [array_ref] reported as a palindrome', Dumper $nok));
+    is(is_palindrome(@$nok), 0, sprintf('(%s) [array] reported as a palindrome',     Dumper $nok));
+}
+
+
+#  Test Undefs
 for  my $undef  (@undef) {
     is(is_palindrome($undef), undef, "item not reported as a undef");
 }
